02840560384458cdfe6b44fb5d38c3e10b65d673
spemer <ghsspower@gmail.com>
2019-03-09 19:15:21 +0900

    bld

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-
 cd docs
 npm run dev
 
 # build
 npm run build
 
-
 cd -
+
+# get commit message
+read -p "Enter commit message: " commitmsg
 
 # commit
 git add .
-git commit -m 'BLD deploy'
+git commit -m $commitmsg
 git push
-
 
 exit
